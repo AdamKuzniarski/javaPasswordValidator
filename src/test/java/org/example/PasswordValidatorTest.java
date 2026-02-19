c3ef98cd5dc3a1d450b59eb3a0648ea561367880
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-02-19 12:13:22 +0100

    create PasswordValidatorTest.java file

---
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @org.junit.jupiter.api.Test
+    void hasMinLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsDigit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsUpperAndLowerCase() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isCommonPassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsSpecialCharacter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isValid() {
+    }
+}
